25c86df8dc7e2c1352c23e2a86f1b9109fecae25
Magda <magda@MacBook-Pro-de-Magda.local>
2015-12-23 17:22:17 -0600

    Cambios para login

---
+++ qapp/Presentation/SignUp.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace qapp
+{
+	public partial class SignUp : ContentPage
+	{
+		public SignUp ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
